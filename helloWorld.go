ce4f20e0de88c7ba73b08f3994187f837e99d935
VladyslavAslanov <vlad.aslanov@icloud.com>
2023-04-10 20:07:51 +0200

    Got mock data

--- helloWorld.go
+++ helloWorld.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-func main ()  {
-	fmt.Println("Hello World")
+func main() {
+	res, err := http.Get("https://cat-fact.herokuapp.com/facts")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	type catFacts struct {
+		Title   string
+		Summary string
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	var data = string(body)
+	var rawData any
+	json.Unmarshal([]byte(data), &rawData)
+	fmt.Println(rawData)
+
 }
